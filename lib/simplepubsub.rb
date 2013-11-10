ce96c7eb467cd527cc82e72de4740ed3eabc9011
James Robertson <james@r0bertson.co.uk>
2013-11-10 21:15:07 +0000

    feature: WebSockets now replaces DRb

--- lib/simplepubsub.rb
+++ lib/simplepubsub.rb
@@ -2,264 +2,126 @@
 
 # file: simplepubsub.rb
 
-require 'open-uri'
-require 'drb'
-require 'dws-registry'
-require 'socket'
-require 'chronic_duration'
+require 'websocket-eventmachine-server'
+require 'websocket-eventmachine-client'
 
-
-USER_AGENT = 'SimplePbSub client 0.5'
 
 module SimplePubSub
 
+  class Server
+
+    def start(options={})
+
+      opt = {host: '0.0.0.0', port: 59000}.merge options
+      host, port = opt[:host], opt[:port]
+
+      EM.run do
+
+        subscribers = {}
+
+        WebSocket::EventMachine::Server.start(host: host, port: port) do |ws|
+
+          ws.onopen do
+            puts "Client connected"
+          end
+
+          ws.onmessage do |msg, type|
+
+            puts "Received message: #{msg}"
+
+            a = msg.split(/\s*:\s*/,2)
+           
+            if a.first == 'subscribe to topic' then
+
+              topic = a.last
+              subscribers[topic] ||= []
+              subscribers[topic] << ws 
+
+            elsif a.length > 1
+
+              puts "publish this %s: %s" % a
+              topic, message = a
+
+              if subscribers[topic] and subscribers[topic].any? then
+
+                connections = subscribers[topic]
+                connections += subscribers['#'] if subscribers['#']
+                connections.each {|c| c.send message }
+              end
+
+            end
+
+            ws.send msg, :type => type
+          end
+
+          ws.onclose do
+            puts "Client disconnected"
+          end
+        end
+
+      end
+    end
+  end
+
   class Client
+
     class PubSub
 
-      class Echo
-
-        def initialize(options, &get_proc)
-          
-          @interval = options[:interval]
-          @t1 = Time.now
-          @get_proc = get_proc
-        end
-
-        def message(topic, message)
-                    
-          return if @interval and @t1 + ChronicDuration.parse(@interval) > Time.now          
-          @t1 = Time.now
-          @get_proc.call topic, message
-        end
-      end
-
-      def initialize(hostname)
-        @hostname = hostname
-      end
+      attr_reader :proc, :topic, :message
 
       def get(topic, options={}, &get_proc)
 
-        DRb.start_service nil, Echo.new(options, &get_proc)
-         
-        obj = DRbObject.new nil, "druby://#{@hostname}:59000"
-        obj.subscribe(topic, DRb.uri)
-        DRb.thread.join        
-
+        @topic = 'subscribe to topic'
+        @proc, @message = get_proc, topic
       end
 
       def publish(topic, message)
 
-        DRb.start_service
-        obj = DRbObject.new nil, "druby://#{@hostname}:59000"
-        obj.deliver topic, message
+        @topic, @message = topic, message
+        @proc = ->(_,_){ :stop}
       end
     end
-
-    attr_reader :remote_obj
-
-    # generally used by the web server
-    #
-    def initialize(hostname)
-      
-      DRb.start_service
-      # attach to the DRb server via a URI given on the command line
-      @remote_obj = DRbObject.new nil, "druby://#{hostname}:59000"      
-    end
     
-    def self.connect(base_url)
-      yield(PubSub.new base_url)
-    end
-  end
-  
-  class Server
-      
-    attr_reader :subscribers, :bridges
-    
-    def initialize(raw_reg='simplepubsub.xml')
+    def self.connect(hostname, port='59000')
 
+      pubsub = PubSub.new
+      yield(pubsub)
 
-      h = {DWSRegistry: ->{raw_reg}, String: ->{DWSRegistry.new raw_reg},:'RSC::Package' => ->{raw_reg}}
+      EM.run do
 
-      @reg = h[raw_reg.class.to_s.to_sym].call
-      
-      doc = Rexle.new(@reg.xml('hkey_apps/simplepubsub'))
+        address = hostname + ':' + port
 
-      root = doc.root
-      # try to read the subscribers
-      topics = root.xpath 'subscription_topics/*'
+        ws = WebSocket::EventMachine::Client.connect(:uri => 'ws://' + address)
 
-      @hostname = Socket.gethostname
-      @subscribers, @bridges = {'#' => []}, {'#' => {}}     
-            
-      topics.each do |topic_element|
-        topic = topic_element.name
-        @subscribers[topic] ||= []
-        @subscribers[topic] = topic_element.xpath 'subscribers/*/text()'
-      end
-
-      @subscribers['#'] = root.xpath 'subscription_all_topics' + \
-          '/subscribers/*/text()'                            
-
-      root.xpath('bridge_topics/*').each do |topic_element|
-        topic = topic_element.name
-        @bridges[topic] = topic_element.elements[0].elements.inject({}) do |r,x|
-          r.merge({x.name.to_s => x.text('address')})
+        ws.onopen do
+          puts "Connected"
         end
-      end
 
+        ws.onmessage do |msg, type|
 
-      @bridges['#'] = root.xpath('bridge_all_topics/subscribers/*')
-          .inject({}) do |r,x|
-        r.merge({x.name.to_s => x.text('address')})
-      end
-
-      'done'
-    end
-
-    def start()
-      
-      # start up the DRb service
-      DRb.start_service 'druby://:59000', self
-
-      # wait for the DRb service to finish before exiting
-      DRb.thread.join
-
-      'done'
-    end
-    
-    def subscribe(topic, uri)
-      
-      topic.sub!('/','_')
-      @subscribers[topic] ||= []
-      @subscribers[topic] << uri      
-      
-      # e.g. 'hkey_apps/simplepubsub/subscription_topics/magic/subscribers/niko', 
-      #         'druby://niko:353524'
-      if topic == '#' then
-        key = "hkey_apps/simplepubsub/subscription_all_topics/subscribers/%s" % \
-          [uri[/[^\/]+$/].sub(':','')]
-      else
-        key = "hkey_apps/simplepubsub/subscription_topics/%s/subscribers/%s" % \
-          [topic, uri[/[^\/]+$/].sub(':','')]        
-      end
-      
-      @reg.set_key key, uri
-    end   
-    
-    def deliver(topic, msg)
-      
-      topic.sub!('/','_')
-      
-      if not @subscribers.include?(topic) and \
-          not @subscribers.include?('#') then
-        return 'no topic subscribers' 
-      end
-                 
-
-      DRb.start_service
-
-      topic_subscribers = @subscribers[topic]
-      
-      if topic_subscribers then
-      
-        topic_subscribers.each do |uri|
-
-          next if @subscribers['#'].include? uri
-          
-          Thread.new {
-            begin          
-              
-              echo = DRbObject.new nil, uri            
-              echo.message topic, msg
-              
-            rescue DRb::DRbConnError => e             
-              
-              @subscribers[topic].delete uri
-              
-              if @subscribers[topic].empty? then
-                @subscribers.delete topic 
-                key = "hkey_apps/simplepubsub/subscription_topics/%s" % [topic]
-              else
-                key = "hkey_apps/simplepubsub/subscription_topics/%s/subscribers/%s" % \
-                    [topic, uri[/[^\/]+$/].sub(':','')]              
-              end
-              
-              @reg.delete_key key
-            end          
-          }
-          
+          a = msg.split(/\s*,\s*/,2)
+          topic, message = a
+          r = pubsub.proc.call topic, message
+          (ws.close; EM.stop) if r == :stop
         end
-      end            
 
-      @subscribers['#'].each do |uri|
-        Thread.new {
-          
-          begin
-            echo = DRbObject.new nil, uri        
-            echo.message topic, msg
-          rescue DRb::DRbConnError => e
-            
-            @subscribers['#'].delete uri
-            key = "hkey_apps/simplepubsub/subscription_all_topics/subscribers/%s" % \
-                  [uri[/[^\/]+$/].sub(':','')]         
-            @reg.delete_key key          
-          end          
-        }
+        ws.onclose do
+          puts "Disconnected"
+        end
+
+        EventMachine.next_tick do
+          ws.send pubsub.topic + ': ' + pubsub.message
+        end
+
 
       end
-    end
-    
-    def add_bridge(topic, hostname, address)
-      
-      if topic == '#' then
-
-        key = "hkey_apps/simplepubsub/bridge_all_topics/webserver/%s/address" % \
-            [hostname]        
-      else
-        topic.sub!('/','_')
-        @bridges[topic] ||= {}
-
-        key = "hkey_apps/simplepubsub/bridge_topics/%s/webserver/%s/address" % \
-            [topic, hostname]
-      end
-      
-      @bridges[topic].merge!(hostname => address)      
-      @reg.set_key key, address    
-    end
-    
-    def delete_bridge(topic, hostname)
-      
-      @bridges[topic].delete hostname
-      
-      if @bridges[topic].empty? and topic != '#' then        
-        @bridges.delete_key(topic)
-        key = "hkey_apps/simplepubsub/bridge_topics/%s" % [topic]
-      else
-        key = "hkey_apps/simplepubsub/bridge_topics/%s/webserver/%s/address" % \
-            [topic, hostname]                
-      end
-      
-      @reg.delete_key key      
-    end
-    
-    def bridge_deliver(topic, message, excluded_host=nil)
-      
-      return 'no matching topic' unless @bridges.has_key? topic
-      
-      if excluded_host  then
-        bridges = @bridges[topic].select{|x| x != excluded_host}
-      else
-        bridges = @bridges[topic]
-      end
-      
-      bridges.values.each do |address|
-        url = "http://%s/do/simplepubsub/bridgepub?topic=%s&hostname=%s&message=%s" % \
-            [address, URI.escape(topic), @hostname, URI.escape(message)]
-        r = open(url, 'UserAgent' => USER_AGENT)
-      end
-      'bridge delivered'
-    end
-    
+    end    
   end
-  
+end
+
+if __FILE__ == $0 then
+
+  # Subscribe example
+  SimplePubSub::Server.new.start
+
 end
--- simplepubsub.gemspec
+++ simplepubsub.gemspec
@@ -1,13 +1,13 @@
 Gem::Specification.new do |s|
   s.name = 'simplepubsub'
-  s.version = '0.5.3'
+  s.version = '1.0.0'
   s.summary = 'simplepubsub'
   s.authors = ['James Robertson']
   s.files = Dir['lib/**/*.rb']
   s.signing_key = '../privatekeys/simplepubsub.pem'
   s.cert_chain  = ['gem-public_cert.pem']  
-  s.add_dependency('dws-registry')
-  s.add_dependency('chronic_duration')
+  s.add_dependency('websocket-eventmachine-server')
+  s.add_dependency('websocket-eventmachine-client')
   s.license = 'MIT'
   s.email = 'james@r0bertson.co.uk'
   s.homepage = 'https://github.com/jrobertson/simplepubsub'
